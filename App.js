c6503a421046802848fdd5ae7cf2e2864476d485
vimal Raveendran <vimalraveendra@gmail.com>
2020-09-02 17:04:48 +0200

    updated App component  with FlatList& Button

--- App.js
+++ App.js
@@ -1,24 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+  View,
+  Image,
+} from 'react-native';
+
+const width = Dimensions.get('window').width;
 
 const App = () => {
+  const [ListItem, setListItem] = useState([]);
+
+  const renderItem = () => {
+    return (
+      <View>
+        <Text>Hello</Text>
+      </View>
+    );
+  };
+
+  const AddImage = () => {};
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Image Picker</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>React Native Add Image</Text>
+      <FlatList
+        data={ListItem}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+        extraData={ListItem}
+      />
+      <TouchableOpacity style={styles.btn} onPress={AddImage}>
+        <Text style={styles.btnText}>Add Image</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 100,
   },
   title: {
-    fontSize: 25,
+    fontSize: 23,
     fontWeight: '700',
+    marginBottom: 10,
+  },
+  btn: {
+    backgroundColor: 'green',
+    height: 50,
+    width: width - 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+    borderRadius: 4,
+  },
+  btnText: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#fff',
   },
 });
 
